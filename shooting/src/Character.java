6f5b4734712786d04e6068f23df29740a7be4a39
haru <zeal23310023@fsgcl.jp>
2024-06-17 12:00:19 +0900

    10,８プレイヤーの絵柄

--- shooting/src/Character.java
+++ shooting/src/Character.java
@@ -6,7 +6,7 @@ public Character(double x,double y,double vx,double vy) {
 	this.x = x;
 	this.y = y;
 	this.vx = vx;
-	this.vy = vx;
+	this.vy = vy;
 }
 public void move() {
 	//
--- shooting/src/GameFrame.java
+++ shooting/src/GameFrame.java
@@ -1,23 +1,39 @@
 import java.util.Vector;
 
-public class GameFrame extends MyFrame{
-	public void run() {
+public class GameFrame extends MyFrame
+{
+	public void run()
+	{
 		GameWorld.player=new Player(100,300,0,0);
 		addKeyListener(GameWorld.player);
 		GameWorld.playerBullets=new Vector<PlayerBullet>();
-		while(true) {
+		while(true) 
+		{
 			clear();
-		}
-		GameWorld.player.draw(this);
-		GameWorld.player.move();
+			GameWorld.player.draw(this);
+			GameWorld.player.move();
+			movePlayerBullets();
+			
+			 sleep(0.03);
+	     }
+	}
+	public void movePlayerBullets() 
+	{
 		int i=0;
-		while (i<GameWorld.playerBullets.size()) {
+		while (i<GameWorld.playerBullets.size()) 
+		{
 			PlayerBullet b=GameWorld.playerBullets.get(i);
 			b.draw(this);
 			b.move();
-			i++;
-		}
-		sleep(0.03);
+			if (b.y<0) 
+			{
+				GameWorld.playerBullets.remove(i);
+			}
+			else 
+			{
+				i++;
+			}
+	       
+	     }
 	}
-
 }
